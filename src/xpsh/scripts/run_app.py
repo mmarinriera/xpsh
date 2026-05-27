1a3082dc9ba282bcc26d7fa933ecb25c9ab83ed3
Miquel Marin Riera <miquel.marin.riera@gmail.com>
2026-05-27 19:44:43 +0200

    fix(app): add entry point

---
+++ src/xpsh/scripts/run_app.py
@@ -0,0 +1,15 @@
+import subprocess
+import sys
+from pathlib import Path
+
+APP_PATH = Path(__file__).with_name("app.py")
+
+
+def main() -> None:
+    file_path = sys.argv[1]
+    print(f"{APP_PATH}: {file_path}")
+    subprocess.run(["streamlit", "run", APP_PATH, file_path])
+
+
+if __name__ == "__main__":
+    main()
