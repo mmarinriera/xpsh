4e9f063be194306ea205bf216a4768fe6b98be2f
Miquel Marin-Riera <miquel.marin.riera@gmail.com>
2026-07-10 15:58:53 +0200

    feat(app): create new ledger (#10)
    When starting the streamlit app, if ledger file path doesn't exist (argv[1]), a new one will be created, using the following passed arguments as ledger members.
    
    If the ledger file path passed exists, ledger is loaded from file, and the following arguments are ignored.

--- src/xpsh/scripts/run_app.py
+++ src/xpsh/scripts/run_app.py
@@ -4,10 +4,23 @@ from pathlib import Path
 
 APP_PATH = Path(__file__).with_name("app.py")
 
+PORT = 8501
+
 
 def main() -> None:
-    file_path = sys.argv[1]
-    subprocess.run([sys.executable, "-m", "streamlit", "run", APP_PATH, "--server.headless", "true", file_path])
+    args = sys.argv[1:]
+    command: list[str] = [
+        sys.executable,
+        "-m",
+        "streamlit",
+        "run",
+        str(APP_PATH),
+        "--server.headless",
+        "true",
+        "--server.port",
+        str(PORT),
+    ] + args
+    subprocess.run(command)
 
 
 if __name__ == "__main__":
